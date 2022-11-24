7411396de6d2b529f0877b0540d2f2261c270a75
chengzihang <czhcheng27@gmail.com>
2022-11-24 12:29:24 +0800

    debounce

---
+++ src/Function/debounce.js
@@ -0,0 +1,35 @@
+// function debounce(callback, time) {
+//   // 定时器变量
+//   let timeId = null;
+//   // 返回一个函数
+//   return function (e) {
+//     // 判断
+//     if (timeId !== null) {
+//       // 清空定时器
+//       clearTimeout(timeId);
+//     }
+//     // 启动定时器
+//     timeId = setTimeout(() => {
+//       // 执行回调
+//       callback.call(this, e);
+//       // 重置计时器变量
+//       timeId = null;
+//     }, time);
+//   };
+// }
+
+// 防抖
+function debounce(fn, delay = 500) {
+  // timer 是闭包中的
+  let timer = null;
+
+  return function () {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, arguments);
+      timer = null;
+    }, delay);
+  };
+}
